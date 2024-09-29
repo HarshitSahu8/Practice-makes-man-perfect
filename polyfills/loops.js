7432d92adde076d322c64edf5b0c5a714007043d
harshitsahu8 <harshitsahu8564@gmail.com>
2024-09-29 21:04:20 +0530

    flat polyfill

--- polyfills/loops.js
+++ polyfills/loops.js
@@ -29,6 +29,26 @@ if (!Array.prototype.map) {
 }
 
 // flat polyfill
-// Array.prototype.flat = function(callback, thisArg) {
+Array.prototype.flat = function (depth = 1) {
+  let output = [];
+  // Helper function to flatten array
+  function flatten(array, depth) {
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i]) && depth > 0) {
+        flatten(array[i], depth - 1); // Recurse with reduced depth
+      } else {
+        output.push(array[i]); // Push non-array elements directly
+      }
+    }
+  }
 
-// }
+  // Start the flattening process with the current array (`this`)
+  flatten(this, depth);
+
+  return output;
+};
+
+const arr = [1, [2], [3, [4]]];
+console.log(arr.flat()); // [1, 2, 3, [4]]
+console.log(arr.flat(1)); // [1, 2, 3, [4]]
+console.log(arr.flat(2)); // [1, 2, 3, 4]
